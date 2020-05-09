46f82e2f27fe63588ad46f23988f903064cf86fb
Wythe Chao <segfaultday@gmail.com>
2020-05-09 14:18:59 +0800

    Initial commit

---
+++ src/main/java/net/jiaozhu/spring/GsSpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package net.jiaozhu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsSpringBootDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/jiaozhu/spring/GsSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jiaozhu.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsSpringBootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
